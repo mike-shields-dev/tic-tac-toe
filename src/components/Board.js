dd0a3a5777e5cd58237438ebfdf6783c97b95a08
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-21 15:27:23 +0100

    Render 9 hardcoded Square components in Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Square from './Square'
 
-export default function Board() {
+export default function Board({squares, onClick}) {
   return (
-    <div>Board</div>
+    <div>
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+      <Square value='1' onClick={() => onClick("dummy value")} />
+    </div>
   )
 }
