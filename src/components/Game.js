43009d600a5edd7acd2b4984920b0dbd9ff50617
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-21 15:32:42 +0100

    Stub out handleClick, jumpTo and renderMoves functions in Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Board from './Board'
 
 export default function Game() {
+  function handleClick() {
+
+  }
+
+  function jumpTo() {
+
+  }
+
+  function renderMoves() {
+    
+  }
   return (
-    <div>Game</div>
+    <Board onClick={handleClick}/>
   )
 }
