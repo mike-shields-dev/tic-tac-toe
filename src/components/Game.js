1106181320404575f93b4686db39d8fc124c3f1b
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-21 16:01:14 +0100

    Create xIsNext state in Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,7 @@ import Board from './Board'
 
 export default function Game() {
   const [board, setBoard] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
   function handleClick() {
     
   }
