fcaf5afbaa0b731ff88c8e17f1d33988cd455666
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-21 16:03:26 +0100

    Import calculateWinner function into Game compent and use it to determine the value of winner variable

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Board from './Board'
+import calculateWinner from '../helpers'
 
 export default function Game() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
+  const winner = calculateWinner(board)
   function handleClick() {
     
   }
