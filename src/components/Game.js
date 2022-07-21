f552422f3b7876cf40738dc043429f1499d0e525
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-21 16:00:45 +0100

    Add board state in Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import { useState } from 'react'
 import Board from './Board'
 
 export default function Game() {
+  const [board, setBoard] = useState(Array(9).fill(null))
   function handleClick() {
     
   }
